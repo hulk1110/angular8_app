0c5dbef0173dda86009aece44dce17ffec95a9e0
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-11-17 10:12:20 +0530

    Two way data binding in angular

--- Angular8App/src/app/app.module.ts
+++ Angular8App/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
 import { UserComponent } from './user/user.component';
@@ -14,7 +14,8 @@ import { ProductComponent } from './product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

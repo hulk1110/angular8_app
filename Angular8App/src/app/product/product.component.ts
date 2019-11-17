59544a1bb8a8f5a6b8bc375416c63997f8f1aad9
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-11-17 10:03:13 +0530

    *ngif demo ANgular 8

--- Angular8App/src/app/product/product.component.ts
+++ Angular8App/src/app/product/product.component.ts
@@ -12,28 +12,28 @@ export class ProductComponent implements OnInit {
 
   constructor() {
 
-    this.products = [
-      {
-        id: 4327,
-        name: 'watch',
-        brand: 'fossil',
-        price: 100000,
-      }, {
-        id: 4328,
-        name: 'ac',
-        brand: 'LG',
-        price: 500000,
-      }, {
-        id: 4329,
-        name: 'Pen',
-        brand: 'MOnt Blanc',
-        price: 800000,
-      }, {
-        id: 4330,
-        name: 'bed',
-        brand: 'Wakefit',
-        price: 1000000,
-      }
+   this.products = [
+    //   {
+    //     id: 4327,
+    //     name: 'watch',
+    //     brand: 'fossil',
+    //     price: 100000,
+    //   }, {
+    //     id: 4328,
+    //     name: 'ac',
+    //     brand: 'LG',
+    //     price: 500000,
+    //   }, {
+    //     id: 4329,
+    //     name: 'Pen',
+    //     brand: 'MOnt Blanc',
+    //     price: 800000,
+    //   }, {
+    //     id: 4330,
+    //     name: 'bed',
+    //     brand: 'Wakefit',
+    //     price: 1000000,
+    //   }
     ];
   }
 
